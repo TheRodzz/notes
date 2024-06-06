8b2682520948cde80f244aa6c6dff9b98cf3657f
Vidhu Arora <vidhu.arora@iiitb.ac.in>
2024-06-06 20:24:34 +0530

    build script added

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,12 @@ app.use(
         origin: "*",
     })
 );
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
+// Handle React routing, return all requests to React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
+});
 
 app.post('/create-account', async (req, res) => {
     const { fullName, email, password } = req.body;
