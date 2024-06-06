afe58f111b9575eca9f986eadb7e716ef7f7e497
Vidhu Arora <vidhu.arora@iiitb.ac.in>
2024-06-06 21:28:31 +0530

    added redirect to dashboard upon empty path

--- frontend/notes/src/App.jsx
+++ frontend/notes/src/App.jsx
@@ -3,13 +3,15 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import SignUp from './pages/SignUp/SignUp'
 import Login from './pages/Login/Login'
-const routes=(
+const routes = (
   <Router>
     <Routes>
-      <Route path='/' exact element={<Home/>}/>
-      <Route path='/dashboard' exact element={<Home/>}/>
-      <Route path='/login' exact element={<Login/>}/>
-      <Route path='/signup' exact element={<SignUp/>}/>
+      <Route exact path="/">
+        <Redirect to="/dashboard" />
+      </Route>
+      <Route path='/dashboard' exact element={<Home />} />
+      <Route path='/login' exact element={<Login />} />
+      <Route path='/signup' exact element={<SignUp />} />
     </Routes>
   </Router>
 )
